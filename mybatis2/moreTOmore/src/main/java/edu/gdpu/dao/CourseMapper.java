d498e8cdc0f2327490094b420beae9f8c7aa50cd
晚霞虽美不如你 <2479537027@qq.com>
2022-04-10 03:28:59 +0000

    删除文件 mybatis2

--- mybatis2/moreTOmore/src/main/java/edu/gdpu/dao/CourseMapper.java
+++
@@ -1,7 +0,0 @@
-package edu.gdpu.dao;
-
-import edu.gdpu.pojo.Course;
-
-public interface CourseMapper {
-    public Course queryCourseByName(String name);
-}
--- mybatis2/moreTOmore/src/main/java/edu/gdpu/dao/StudentMapper.java
+++
@@ -1,7 +0,0 @@
-package edu.gdpu.dao;
-
-import edu.gdpu.pojo.Student;
-
-public interface StudentMapper {
-    public Student queryStudentById(int id);
-}
